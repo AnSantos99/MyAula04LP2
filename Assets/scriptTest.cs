232cd5bbbc926a2e629e275c2fe9cebf47208323
AnSantos99 <44712120+AnSantos99@users.noreply.github.com>
2019-10-16 16:54:25 +0100

    Co-Routines

---
+++ Assets/scriptTest.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scriptTest : MonoBehaviour
+{
+
+    private Coroutine helloRot;
+
+    WaitForSeconds wfs = new WaitForSeconds(4);
+
+    WaitForSeconds wfss = new WaitForSeconds(1);
+
+    private IEnumerator Hello() 
+    {
+        
+
+        while (true) 
+        {
+            yield return wfs;
+
+            Debug.Log("Hello");
+        }
+    }
+
+    private IEnumerator Counter() 
+    {
+        WaitForSeconds wfss = new WaitForSeconds(1);
+        int m = 0;
+
+        while (true) 
+        {
+            Debug.Log(m);
+            m++;
+
+            if (m > 30)
+                StopCoroutine(helloRot);
+            yield return wfss;
+        }
+    }
+
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        helloRot = StartCoroutine(Hello());
+        StartCoroutine(Counter());
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        
+    }
+}
